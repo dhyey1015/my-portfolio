108d0c0b862e0ff99f67645b3f4963815f160d37
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-19 20:38:19 +0530

    tailwind css integrated

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { AppBar } from './assets/components/AppBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      hi there!!
+      <AppBar/>
     </div>
   )
 }
