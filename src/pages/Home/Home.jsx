a202e407cc39d713f2448dbc6cf35ea98070e68a
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-11-21 15:50:14 +0530

    doen with adding (what i do?) part in the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 // import myProfile from "public/images/myprofile.png"
 
 import { HomeButton } from "./ButtonHome";
-import { GithubIcon, LinkedInIcon } from "./SvgsHome";
+import { BulletPointIcon, GithubIcon, LinkedInIcon } from "./SvgsHome";
 
 export function Home(){
     return(
@@ -10,12 +10,12 @@ export function Home(){
                 {/*start profile and introduction */}
                 <div className="flex justify-between p-10">
                     <div className="pr-16">
-                        <img className="rounded-full h-[500px] w-[500px]" src="images/myprofile.png" alt="" />
+                        <img className="rounded-full h-[350px] w-[350px]" src="images/myprofile.png" alt="" />
                     </div>
                     <div className="pt-10 ">
                         <div class="h-[400px] min-h-[1em] w-[2px] self-stretch bg-gradient-to-tr from-transparent bg-slate-300 to-transparent opacity-25 dark:bg-slate-800"></div>
                     </div>
-                    <div className="flex items-center pl-16 w-[500px]">
+                    <div className="flex items-center pl-16 w-[350px]">
                         <div className="text-left">
                             <span className="text-xl">
                                 Hi, there
@@ -58,23 +58,55 @@ export function Home(){
             <div className="flex justify-center">
                 {/*start profile and introduction */}
                 <div className="flex justify-between p-10">
-                    <div className="pr-16">
-                        <img className="rounded-full h-[500px] w-[500px]" src="images/myprofile.png" alt="" />
-                    </div>
-                    <div className="pt-10 ">
-                        <div class="h-[400px] min-h-[1em] w-[2px] self-stretch bg-gradient-to-tr from-transparent bg-slate-300 to-transparent opacity-25 dark:bg-slate-800"></div>
-                    </div>
-                    <div className="flex items-center pl-16 w-[500px]">
+                <div className="flex items-center pl-16 w-[500px]">
                         <div className="text-left">
                             <span className="font-bold text-4xl">
                                 Full Stack Development
                             </span>
                             <br />
                             <br />
-                            <span>
-                                
+                            <span className="flex justify-start">
+                                <div className="pi-3 pr-4">
+                                    <BulletPointIcon/> 
+                                </div>
+                                <div className="text-xl">
+                                    Good in creating beautiful and responsive UI's using React.js and Tailwind CSS
+                                </div>
+                            </span>
+                            <br />
+                            <span className="flex justify-start">
+                                <div className="pi-3 pr-4">
+                                    <BulletPointIcon/> 
+                                </div>
+                                <div className="text-xl">
+                                    Skilled in building server-side applications with Express.js and Node.js
+                                </div>
+                            </span>
+                            <br />
+                            <span className="flex justify-start">
+                                <div className="pi-3 pr-4">
+                                    <BulletPointIcon/> 
+                                </div>
+                                <div className="text-xl">
+                                    Good at integragting front-end and back-end for fully functional website
+                                </div>
+                            </span>
+                            <br />
+                            <span className="flex justify-start">
+                                <div className="pi-3 pr-4">
+                                    <BulletPointIcon/> 
+                                </div>
+                                <div className="text-xl">
+                                    Skilled in using Django for rapid bakend development and managing Databases
+                                </div>
                             </span>
                         </div> 
+                    </div>
+                    <div className="pt-10 ">
+                        <div class="h-[400px] min-h-[1em] w-[2px] self-stretch bg-gradient-to-tr from-transparent bg-slate-300 to-transparent opacity-25 dark:bg-slate-800"></div>
+                    </div>
+                    <div className="">
+                        <img className="h-[400px] w-[500px] pt-20" src="images/fullsatcksideimage4.png" alt="" />
                     </div>
                 </div>
                 {/*end profile and introduction */}
