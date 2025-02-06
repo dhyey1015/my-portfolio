9348a228a318bbbcf5e11b29c7e4c286f251fd08
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-02-06 22:21:40 +0530

    removed unnecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { AppBar } from './components/Header/AppBar'
 import { Footer } from './components/Footer/Footer'
